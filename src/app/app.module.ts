464851efa2ceb509a0caae39c6658c1b2f5e98b4
Arpan Gupta <gupta-arpan@pop-os.localdomain>
2023-08-17 16:11:25 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list-edit/shopping-list-edit.component';
+import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
